83a7addfad94233ee72c45a41be6b1c70f7fe89e
Diego Sales <diegosgjoao@gmail.com>
2025-05-28 22:16:19 -0300

    feat: getters and setters

--- src/main/java/com/bdfatecdiego/model/Endereco.java
+++ src/main/java/com/bdfatecdiego/model/Endereco.java
@@ -5,4 +5,38 @@ public class Endereco {
     private String estado;
     private String cidade;
     private String cep;
+
+    public void Endereco() {};
+
+    public void setEnderecoCompleto(String estado, String cidade, String cep) {
+        this.enderecoCompleto = cidade + ", " + estado + "-" + cep;
+    }
+
+    public String getEnderecoCompleto() {
+        return enderecoCompleto;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCep() {
+        return cep;
+    }
 }
--- src/main/java/com/bdfatecdiego/model/Seguro.java
+++ src/main/java/com/bdfatecdiego/model/Seguro.java
@@ -6,4 +6,46 @@ public abstract class Seguro {
     private Endereco endereco;
     private float valor;
     private float premio;
+
+    public void Seguro() {};
+
+    public void setNumApolice(int numApolice) {
+        this.numApolice = numApolice;
+    };
+
+    public int getNumApolice() {
+        return numApolice;
+    };
+
+    public void setNome(String nome) {
+        this.nome= nome;
+    };
+
+    public String getNome() {
+        return nome;
+    };
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    };
+
+    public Endereco getEndereco() {
+        return endereco;
+    };
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    };
+
+    public float getValor() {
+        return numApolice;
+    };
+
+    public void setPremio(float premio) {
+        this.premio = premio;
+    };
+
+    public float getPremio() {
+        return premio;
+    };
 }
